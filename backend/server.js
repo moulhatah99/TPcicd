4655bcca768f08d6cca5e85f40a521b7cd053015
moulhatah99 <moulhatahmed@email.com>
2024-03-13 18:41:44 +0100

    projet cicd

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+module.exports = app; // Exporter l'application pour les tests
